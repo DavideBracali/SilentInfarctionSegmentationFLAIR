f2f8e6bcf94d895ce051241f39f9c45ad77072e0
DavideBracali <davide.bracali@studio.unibo.it>
2025-05-29 17:10:15 +0200

    [WORK] setup and toml

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ __version__ = '{version}'
 '''
 
   filename = os.path.abspath(os.path.dirname(__file__))
-  filename = f'{filename}/pyhide/__version__.py'
+  filename = f'{filename}/SilentInfarctionSegmentationFLAIR/__version__.py'
 
   with open(filename, 'w') as fp:
     fp.write(script)
@@ -45,7 +45,7 @@ AUTHOR = 'Davide Bracali'
 EMAIL = 'davide.bracali@studio.unibo.it'
 REQUIRES_PYTHON = '>=3.5'
 PACKAGE_VERSION = '0.0.1'
-DESCRIPTION = '°_°'
+DESCRIPTION = '3D segmentation of silent infarctions in FLAIR MRI images.'
 URL = 'https://github.com/DavideBracali/SilentInfarctionSegmentationFLAIR'
 MAINTAINER = AUTHOR
 MAINTAINER_EMAIL = EMAIL
@@ -77,22 +77,20 @@ setup(
   },
   include_package_data=True,
   platforms='any',
-  classifiers=[
-    'Natural Language :: English',
-    'Operating System :: MacOS :: MacOS X',
-    'Operating System :: POSIX',
-    'Operating System :: POSIX :: Linux',
-    'Operating System :: Microsoft :: Windows',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: 3.11'
-  ],
+  classifiers = [
+  'Natural Language :: English',
+  'Operating System :: MacOS :: MacOS X',
+  'Operating System :: POSIX',
+  'Operating System :: POSIX :: Linux',
+  'Operating System :: Microsoft :: Windows',
+  'Programming Language :: Python',
+  'Programming Language :: Python :: 3',
+  'Programming Language :: Python :: 3.11',
+  'Programming Language :: Python :: 3.12',
+  'Programming Language :: Python :: 3.13',
+  'Programming Language :: Python :: Implementation :: CPython',
+  'Programming Language :: Python :: Implementation :: PyPy']
+,
   entry_points={'console_scripts': [
     'SilentInfarctionSegmentationFLAIR = SilentInfarctionSegmentationFLAIR.__main__:main',
     ],
