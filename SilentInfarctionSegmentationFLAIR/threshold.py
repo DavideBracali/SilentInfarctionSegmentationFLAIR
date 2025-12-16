c01a4741782d4198e89e2e8eb0563f8789ea6047
DavideBracali <davide.bracali@studio.unibo.it>
2025-12-16 11:29:19 +0100

    "[ENH] cleaner and simpler code"

--- SilentInfarctionSegmentationFLAIR/threshold.py
+++ SilentInfarctionSegmentationFLAIR/threshold.py
@@ -17,8 +17,7 @@ from SilentInfarctionSegmentationFLAIR.segmentation import apply_threshold
 from SilentInfarctionSegmentationFLAIR.segmentation import evaluate_voxel_wise
 from SilentInfarctionSegmentationFLAIR.refinement import evaluate_region_wise
 
-def main(image, gm, gamma, gt=None, show=True, verbose=True,
-         save_hist_path=None, save_metrics_path=None):
+def main(image, gm, gamma, show=True, verbose=True, save_path=None):
 
     # evaluation parameters
     metrics_rw = []
@@ -32,14 +31,7 @@ def main(image, gm, gamma, gt=None, show=True, verbose=True,
         ax = None
 
     # compute gm histogram
-    if gt is not None:
-        lesions = sitk.Mask(image, gt)
-        hists = plot_multiple_histograms([gm, lesions], no_bkg=True, show=show, ax=ax,
-                                         labels=["Gray matter","Lesions"], normalize=True,
-                                         bins=[None,'auto'])
-        gm_hist = hists[0]
-    else:
-        gm_hist = plot_histogram(gm, no_bkg=True, bins='fd', show=show, ax=ax)
+    gm_hist = plot_histogram(gm, no_bkg=True, bins='fd', show=show, ax=ax)
     
     # smooth histogram with gaussian filter
     gm_smooth_hist = gaussian_smooth_histogram(gm_hist, show=show)
@@ -47,23 +39,19 @@ def main(image, gm, gamma, gt=None, show=True, verbose=True,
     # find mode and right-side half width at half maximum
     mode, rhwhm = mode_and_rhwhm(gm_smooth_hist, show=show)
 
+    # apply threshold
     thr = mode + gamma * rhwhm
+    if verbose:
+        print(f"Applying threshold at gray level {thr:.1f} (gamma = {gamma:.1f})")
     thr_mask = apply_threshold(image, float(thr), show=show)
 
     # plot additional details
-    if show:
-        ax.legend()
-        ax.set_title(f"GM histogram and threshold with γ={gamma}")
-        plt.tight_layout()
-        plt.show()
+    ax.legend()
+    ax.set_title(f"GM histogram and threshold with γ={gamma}")
+    plt.tight_layout()
+    
+    if save_path is not None:   plt.savefig(save_path)
 
-    # evaluate
-    if gt is not None:
-        metrics_rw = evaluate_region_wise(thr_mask, gt)
-        metrics_vw = evaluate_voxel_wise(thr_mask, gt)
-        
-        if verbose:
-            print(f"Region-wise DICE coefficient after thresholding: {metrics_rw['DSC']:.4f}")
-            print(f"Voxel-wise DICE coefficient after thresholding: {metrics_vw['DSC']:.4f}")
+    if show:    plt.show()
 
     return thr_mask
